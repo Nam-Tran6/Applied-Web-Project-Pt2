fc60540468df0447107b2496c6404ac03e640d39
Kha Nam Tran <104555355@student.swin.edu>
2025-10-19 22:53:31 +1100

    update link + alt comments + images + add new title for Index page

--- index.php
+++ index.php
@@ -17,86 +17,114 @@
     <link rel="stylesheet" href="styles/layout.css" type="text/css">
 
     <style>
+        /* CSS selection for class categorised "items"*/
         .item {
-            /* selects all class of items*/
+            /* CSS styling for class "item" to have a #ccc background and margin/padding changes*/
             margin: 8em;
             padding: 3em;
             color: #1a1a1a;
             background-color: #ccc;
-            border-radius: 1em;
-            /* Defines margin, padding, font color, background color, and radius for rectangle edges*/
         }
 
+        /*CSS for body*/
         body {
+            /* Background illustration, sourced from Adobe Stock + generated with AI
+            Link: https://stock.adobe.com/images/abstract-plexus-network-connection-background-with-digital-data-and-technology/1525752799?prev_url=detail*/
             background: url(styles/images/background.png);
-            /* Background illustration*/
-            background-size: cover
-                /* Background cover*/
-
-            /* Image was sourced from Adobe Stock and generated with AI 
-            Link: https://stock.adobe.com/images/abstract-plexus-network-connection-background-with-digital-data-and-technology/1525752799?prev_url=detail
-            */
+            
+            /* Background cover*/
+            background-size: cover;
         }
 
-        /*styling for heading*/
-        #CatchyPhrase h1, {
+        /*CSS for section with ID of "FrontTitle"*/
+        #FrontTitle {
             color: #ccc;
-            padding: 10px 10px;
-            margin: 5%;
+            background-color: #141414;
+            margin: 1em 6em 1em 6em; 
+            padding: 3em 1em 3em 1em; 
             text-align: center;
-            border-radius: 10px;
+            border-radius: 1em;
+            font-size: 2em;
+
+            /*Converts section into 2 columns with grid*/
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+        }
+
+        /*Selects all sections within the article semantic*/
+        article {
+            /* utilises grid to produce 2 columns and 2 rows*/
+            display: grid;
+            grid-template-columns: repeat(2, 1fr);
+            grid-template-rows: repeat(2, 1fr);
+        }
+
+        #cloudpicture {
+            text-align: center;
+            margin: 1em;
         }
     </style>
 </head>
 
 <body>
     <?php
-        // header inclusions
+        // .inc header inclusion
         include "header.inc";
     ?>
-
-    <main>
-        <!--Heading containing catchy phrase and description-->
-        <section id="CatchyPhrase">
-            <h1>
-                Welcome to The Box&#8482;
-            </h1>
-
-            <p>
-                Not Gigabytes, Terrabytes, or even Petabyte. Think as bold as us, think Yottabyte.
-            </p>
-        </section>
-
+    
+<main>
+    <section id = "cloudpicture">
+        <img src="images/Box.png" alt="Image of a cloud streching out network connections, 
+        enforcing our brand as a storage site">
+    </section>
+    <!--Image of cloud storage to illustrate business plan-->
+    
         
-        <!--Description 1-->
+    <!--Section for Front-Title-->
+    <section id="FrontTitle">
+    
+    <!--Heading introducing user to the business-->
+    <h1>
+        Welcome to <br> 
+        &lt;--- The Box&#8482; ---&gt;
+    </h1>
+
+    <!--Short introductionary paragraph with inline CSS to override external CSS rule-->
+        <p style = "color: grey;">
+            A new innovative tech-startup that utilises quantum technology to produce an unfathomable amount of storage space
+        </p>
+    </section>
+
+    <article>
+        <!--Top Left Description detailing The Box business-->
         <section class="item">
             <h2>What is the BOX?</h2>
             <p> A new method to store data, utilising cutting edge innovations from quantum mechanics to expand into the
                 infinite realm of data storage. </p>
         </section>
 
-        <!--Description 2-->
+        <!--Top Right Description detailing the team behind The Box-->
         <section class="item">
             <h2>Who are We?</h2>
             <p> We are a cutting-edge team of programmers and engineers who wish to create the building blocks of a new
                 way to store. </p>
         </section>
 
-        <!--Description 3-->
+        <!--Bottom Left Description detailing what The Box offers-->
         <section class="item">
             <h2>What do we offer?</h2>
-
             <p> We operate on both a B2B and B2P model, providing every consumer, no matter how big or small, the
                 opportunity to access our infinite servers. </p>
         </section>
 
-        <!--Description 4-->
+        <!--Bottom Right Description detailing liabilities-->
         <section class="item">
             <h2>Pandora's Box</h2>
             <p> The BOX is not liable if somehow, we open an interdimensional rift into some kind of wierd data world.
                 We would happily worship our Data Overloads though. </p>
         </section>
-    </main>
+    </article>
+</main>
 
     <?php
         // header footer

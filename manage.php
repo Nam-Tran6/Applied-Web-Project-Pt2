a099e5101014609d3c9321e337c112a0c155d79c
KuySothearith <Sothearith.kuy37@gmail.com>
2025-10-26 13:12:54 +1100

    apply login logic to the manage.php by user have to login suceess to see the mange.php otherwise it will show the login.php

--- manage.php
+++ manage.php
@@ -1,2 +1,16 @@
+<?php
+session_start();
+include "settings.php"; // Connects to your database
 
-
+//  Check if user is logged in, otherwise redirect to login page
+if (!isset($_SESSION['user'])) {
+    header("Location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Hello <?php echo htmlspecialchars($_SESSION['user']); ?></h1>
+</body>
+</html>

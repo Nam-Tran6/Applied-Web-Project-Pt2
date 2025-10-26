23e553984d5414b4a705b1592e198c53360d856e
KuySothearith <Sothearith.kuy37@gmail.com>
2025-10-26 20:22:11 +1100

    move database conn code to login.php and manage.php

--- settings.php
+++ settings.php
@@ -4,9 +4,4 @@ $user = "root";
 $pwd = "";
 $sql_db = "database"; 
 
-$conn = new mysqli($host, $user, $pwd, $sql_db);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
+?>

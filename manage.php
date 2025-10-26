2532d83dc5d6dcb18933aeb7e10f75b28258f4d1
KuySothearith <Sothearith.kuy37@gmail.com>
2025-10-26 13:20:45 +1100

    create logout.php page

--- manage.php
+++ manage.php
@@ -2,7 +2,7 @@
 session_start();
 include "settings.php"; // Connects to your database
 
-//  Check if user is logged in, otherwise redirect to login page
+// Check if user is logged in, otherwise redirect to login page
 if (!isset($_SESSION['user'])) {
     header("Location: login.php");
     exit;
@@ -10,7 +10,14 @@ if (!isset($_SESSION['user'])) {
 ?>
 <!DOCTYPE html>
 <html>
+<head>
+    <title>Manage Page</title>
+</head>
 <body>
-<h1>Hello <?php echo htmlspecialchars($_SESSION['user']); ?></h1>
+    <h1>Hello <?php echo htmlspecialchars($_SESSION['user']); ?></h1>
+    
+    <form action="logout.php" method="post">
+        <button type="submit">Logout</button>
+    </form>
 </body>
 </html>

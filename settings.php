0b018e3b715f8fe863c6cd4b71e3e014c96f615f
KuySothearith <Sothearith.kuy37@gmail.com>
2025-10-26 11:46:26 +1100

    apply new login li in navbar on every page

--- settings.php
+++ settings.php
@@ -4,4 +4,9 @@ $user = "root";
 $pwd = "";
 $sql_db = "database"; 
 
+$conn = new mysqli($host, $user, $pwd, $sql_db);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 ?>
